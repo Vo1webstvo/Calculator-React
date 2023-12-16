e7f04c1c6af5aa1327cffd2ead7a34399f829863
Evgeniy <evgen1y-igorevich@yandex.com>
2023-12-16 17:48:33 +0300

    refactor: закончил логику вычислений

--- src/Components/Buttons/Buttons.jsx
+++ src/Components/Buttons/Buttons.jsx
@@ -7,7 +7,7 @@ const Buttons = () => {
   const [action, setAction] = useState('');
   const [finish, setFinish] = useState(false);
 
-  const numberValue = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ','];
+  const numberValue = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
   const actionValue = ['+', 'x', '-', '/', '%'];
 
   const clearAll = () => {
@@ -15,6 +15,16 @@ const Buttons = () => {
     setRes1('');
     setRes2('');
     setAction('');
+    setFinish(false);
+  };
+
+  const reverseNumber = (num) => {
+    if (num === '0') return;
+    if (num > '0') {
+      return -num;
+    } else {
+      return Math.abs(num);
+    }
   };
 
   const getChangeValue = (e) => {
@@ -22,65 +32,135 @@ const Buttons = () => {
 
     // кнопки 0-9
     if (numberValue.includes(res)) {
-      if (!res1 && !action) {
+      if (!res2 && !action) {
         setRes1((res1) => res1 + res);
-        // setVisibleValue((prev) => (prev += res));
+        setVisibleValue((prev) => (prev += res));
+      } else if (res1 && res2 && finish) {
+        setRes2(res);
+        setFinish(false);
+        setVisibleValue(res2);
+      } else {
+        setRes2((prev) => prev + res);
+        setVisibleValue(res2);
+        return;
       }
-      // if (res1 && res2 && finish) {
-      //   setRes2((prev) => prev + res);
-      //   setFinish(false);
-      //   setVisibleValue(res2);
-      // }
-      // if (res1 && action) {
-      //   setRes2((prev) => prev + res);
-      //   setVisibleValue(res2);
-      // }
     }
 
-    //кнопки +, - , / , x, %
-    // if (actionValue.includes(res)) {
-    //   setAction(res);
-    //   setVisibleValue(action);
-    // }
+    // кнопки +, - , / , x, %
+    if (actionValue.includes(res)) {
+      setAction(res);
+      setVisibleValue(action);
+    }
+
+    if (res === '+-') {
+      if (!res2 && !action) {
+        setRes1(reverseNumber(res1));
+      } else if (res1 && res2 && finish) {
+        setRes1(reverseNumber(res1));
+      } else {
+        setRes2(reverseNumber(res2));
+      }
+    }
+
+    //если нажимаем знак а не равно, т.е. 1+1+1+2+1 и т.д
+    if (res1 && res2 && action && !finish) {
+      if (res === '+' || res === '-' || res === 'x' || res === '/') {
+        switch (action) {
+          case '+':
+            setRes1(+res1 + +res2);
+            break;
+          case '-':
+            setRes1(+res1 - +res2);
+            break;
+          case 'x':
+            setRes1(+res1 * +res2);
+            break;
+          case '/':
+            if (res2 === '0' || res2 === 0) {
+              setRes1('error');
+              break;
+            }
+            setRes1(+res1 / +res2);
+            break;
+        }
+        setFinish(true);
+        setVisibleValue(res1);
+      }
+    }
+
+    //если нажимает = сразу, т.е. 1+1=
+    if (res === '=') {
+      if (!res2) setRes2(res1);
+      switch (action) {
+        case '+':
+          setRes1(+res1 + +res2);
+          break;
+        case '-':
+          setRes1(+res1 - +res2);
+          break;
+        case 'x':
+          setRes1(+res1 * +res2);
+          break;
+        case '/':
+          if (res2 === '0' || res2 === 0) {
+            setRes1('error');
+            break;
+          }
+          setRes1(+res1 / +res2);
+          break;
+      }
+      setFinish(true);
+      setVisibleValue(res1);
+    }
+
+    //нажимаем %
+    if (res === '%') {
+      if (!res2) setRes2(res1);
+      switch (action) {
+        case '+':
+          setRes1((+res1 / 100) * +res2 + +res2);
+          break;
+        case '-':
+          setRes1((+res1 / 100) * +res2 - +res2);
+          break;
+        case 'x':
+          setRes1((+res2 / 100) * +res1);
+          break;
+        case '/':
+          if (res2 === '0' || res2 === 0) {
+            setRes1('error');
+            break;
+          }
+          setRes1(+res1 / res2) * res2;
+          break;
+      }
+      setVisibleValue(res1);
+    }
   };
 
-  // if (res1 && res2) {
-  //   switch (action) {
-  //     // case '+':
-  //     //   setRes1(+res1 + +res2);
-  //     //   setVisibleValue(+res1 + +res2);
-  //     // case 'x':
-  //     //   setRes1(+res1 * +res2);
-  //     //   setVisibleValue(+res1 * +res2);
-  //     case '-':
-  //       setRes1(res1 - res2);
-  //       setVisibleValue(res1 - res2);
-  //     // case '/':
-  //     //   setRes1(+res1 / +res2);
-  //     //   setVisibleValue(+res1 / +res2);
-  //     // case '=':
-  //     //   setRes1(+res1 + +res2);
-  //     //   setVisibleValue(+res1 + +res2);
-  //   }
-  // }
+  useEffect(() => {
+    setVisibleValue(res1);
+  }, [res1]);
 
-  // useEffect(() => {
-  //   setVisibleValue(res1);
-  // }, [res1]);
+  useEffect(() => {
+    setVisibleValue(res2);
+  }, [res2]);
 
-  // useEffect(() => {
-  //   setVisibleValue(res2);
-  // }, [res2]);
+  useEffect(() => {
+    setVisibleValue(action);
+  }, [action]);
 
   return (
     <>
-      <input className="inputs old" type="text" value="oldres" />
-      <input className="inputs new" type="text" value={visibleValue} />
+      {/* <input className="inputs old" type="text" value="oldres" /> */}
+      <input className="inputs new" type="text" defaultValue={visibleValue} placeholder="0" />
       <div className="wrapper" onClick={(e) => getChangeValue(e)}>
         <button className="wrapper__btn" onClick={clearAll}>
           C
         </button>
-        <button className="wrapper__btn">+-</button>
+        <button className="wrapper__btn" onClick={reverseNumber}>
+          +-
+        </button>
         <button className="wrapper__btn">%</button>
         <button className="wrapper__btn-red">/</button>
         <button className="wrapper__btn">7</button>
@@ -96,7 +176,7 @@ const Buttons = () => {
         <button className="wrapper__btn">3</button>
         <button className="wrapper__btn-red">+</button>
         <button className="wrapper__btn">0</button>
-        <button className="wrapper__btn">,</button>
+        <button className="wrapper__btn">.</button>
         <button className="wrapper__btn-yellow">=</button>
       </div>
     </>
